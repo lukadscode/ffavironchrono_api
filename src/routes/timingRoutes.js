16bab7958254c37fb4d6a88ab720847c73adbd45
luka <dasilva.luka97@gmail.com>
2026-01-04 10:56:51 +0100

    update

--- src/routes/timingRoutes.js
+++ src/routes/timingRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/timingController");
-const timingPointAuth = require("../middlewares/timingPointAuthMiddleware");
+const flexibleAuth = require("../middlewares/flexibleAuthMiddleware");
 
 router.get("/", controller.getTimings);
-router.post("/", timingPointAuth, controller.createTiming);
+router.post("/", flexibleAuth, controller.createTiming);
 // Routes spécifiques avant les routes génériques
 router.get("/event/:event_id", controller.getTimingsByEvent);
 router.get("/race/:race_id", controller.getTimingsByRace);
 // Routes génériques après les routes spécifiques
 router.get("/:id", controller.getTiming);
-router.put("/:id", timingPointAuth, controller.updateTiming);
-router.delete("/:id", timingPointAuth, controller.deleteTiming);
+router.put("/:id", flexibleAuth, controller.updateTiming);
+router.delete("/:id", flexibleAuth, controller.deleteTiming);
 
 module.exports = router;
